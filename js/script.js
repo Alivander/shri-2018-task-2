9ff98f88217dc07b9c1127ea9a8af81898a00eda
Alina Vanieva <vanieva.alina@yandex.ru>
2018-01-08 02:43:46 +0400

    Оживление тултипов с названием переговорки

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-var events = document.querySelectorAll(".diagram__event");
+var events = document.querySelectorAll(".floor__event");
+
 var eventOnClick = function (item) {
   item.addEventListener("click", function (evt) {
     evt.preventDefault();
-    item.classList.toggle("diagram__event--active");
+    item.classList.toggle("floor__event--active");
   });
 };
 
@@ -12,3 +13,28 @@ for (var i = 0; i < events.length; i++) {
   eventTooltip.style.marginLeft = eventLeft + "px";
   eventOnClick (events[i]);
 };
+
+i = 0;
+
+var diagram = document.querySelector(".diagram");
+var room = diagram.querySelector(".floor__room");
+var roomTitles = diagram.querySelectorAll(".floor__room-title");
+
+var roomOnScroll = function (items) {
+  if (window.pageXOffset > room.offsetWidth) {
+    for (var i = 0; i < items.length; i++) {
+      items[i].classList.add("floor__room-title--tooltip");
+      items[i].style.left = window.pageXOffset + 12 + "px";
+    };
+  } else {
+    for (var j = 0; j < roomTitles.length; j++) {
+      if (items[j].classList.contains("floor__room-title--tooltip")) {
+        items[j].classList.remove("floor__room-title--tooltip");
+      };
+    };
+  };
+};
+
+window.addEventListener("scroll", function () {
+  roomOnScroll(roomTitles);
+});
