cac0f93b3d6b98e7e4f05b6c1ce680c3f85d9891
Alina Vanieva <vanieva.alina@yandex.ru>
2018-01-10 16:56:03 +0400

    Реализация простого открытия страницы встречи и закрытия

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//Появление тултипа с информацией о встрече
+
 var events = document.querySelectorAll(".floor__event");
 
 var eventOnClick = function (item) {
@@ -14,7 +16,8 @@ for (var i = 0; i < events.length; i++) {
   eventOnClick (events[i]);
 };
 
-i = 0;
+
+//Появление плавающего тултипа с названием переговорки
 
 var diagram = document.querySelector(".diagram");
 var room = diagram.querySelector(".floor__room");
@@ -39,11 +42,14 @@ window.addEventListener("scroll", function () {
   roomOnScroll(roomTitles);
 });
 
+
+//Реализация открытия календаря на главной странице
+
 var date = document.querySelector(".date");
 var dateActive = date.querySelector(".date__active-date");
 var dateCalendar = date.querySelector(".calendar");
 
-dateActive.addEventListener("click", function() {
+dateActive.addEventListener("click", function () {
   dateCalendar.classList.toggle("date__calendar--shown");
 });
 
@@ -55,25 +61,56 @@ dateCalendar.addEventListener("click", function (event) {
   }
 });
 
-var event = document.querySelector(".event");
-var eventCalendarControl = event.querySelector(".event__calendar-contol");
-var eventCalendar = event.querySelector(".calendar");
 
-eventCalendarControl.addEventListener("click", function() {
+//Реализация окрытия календаря на странице встречи
+
+var eventPage = document.querySelector(".event");
+var eventCalendarControl = eventPage.querySelector(".event__calendar-contol");
+var eventCalendar = eventPage.querySelector(".calendar");
+
+eventCalendarControl.addEventListener("click", function () {
   eventCalendar.classList.toggle("event__calendar--shown");
 });
 
-eventCalendar.addEventListener("click", function (event) {
-  if (event.target.classList.contains("calendar__day-number")) {
+eventCalendar.addEventListener("click", function (evt) {
+  if (evt.target.classList.contains("calendar__day-number")) {
     setTimeout(function () {
       eventCalendar.classList.remove("event__calendar--shown");
     }, 350);
   }
 });
 
-var eventOptionsControl = event.querySelector(".event__options-control");
-var eventOptions = event.querySelector(".event__options");
 
-eventOptionsControl.addEventListener("click", function() {
+//Реализация открытия списка возможных участников на странице всречи
+
+var eventOptionsControl = eventPage.querySelector(".event__options-control");
+var eventOptions = eventPage.querySelector(".event__options");
+
+eventOptionsControl.addEventListener("click", function () {
   eventOptions.classList.toggle("event__calendar--shown");
+});
+
+
+// Реализация открытия страницы с созданием встречи
+
+var headerButton = document.querySelector(".header__button");
+var mainPage = document.querySelector(".page__main");
+var eventRecomendation = eventPage.querySelector(".event__recommendation");
+
+headerButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (!eventPage.classList.contains("event--shown")) {
+    eventPage.classList.add("event--shown");
+  }
+  headerButton.classList.add("header__button--hidden");
+  eventRecomendation.classList.add("event__recommendation--hidden");
+});
+
+//Закрытие страницы с встречей без сохранения
+
+var eventClose = eventPage.querySelector(".event__button[type=reset]");
+
+eventClose.addEventListener("click", function () {
+  eventPage.classList.remove("event--shown");
+  headerButton.classList.remove("header__button--hidden");
 });
